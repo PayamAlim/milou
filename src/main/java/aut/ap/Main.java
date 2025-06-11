9807dfe4026b48984a07dc35bf65404ac9d86148
Payam <payam85ar@gmail.com>
2025-06-11 23:01:34 +0330

    Add UserService

--- src/main/java/aut/ap/Main.java
+++ src/main/java/aut/ap/Main.java
@@ -1,11 +1,53 @@
 package aut.ap;
-import java.sql.Timestamp;
-import java.time.LocalDateTime;
+import aut.ap.model.*;
+import aut.ap.service.UserService;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Timestamp createAt;
-        createAt = Timestamp.valueOf(LocalDateTime.now());
-        System.out.println(createAt);
+        Scanner scn = new Scanner(System.in);
+
+        System.out.println("[L]ogin, [S]ign up:");
+        while (true) {
+            String command = scn.nextLine().trim().toLowerCase();
+
+            if ("l".equals(command) || "login".equalsIgnoreCase(command)) {
+                System.out.print("Email: ");
+                String email = scn.nextLine().trim();
+
+                System.out.print("Password: ");
+                String password = scn.nextLine().trim();
+
+                try {
+                    User user = UserService.loginUser(email, password);
+                    System.out.println("Welcome back, " + user.getName() + "!");
+                } catch (Exception e) {
+                    System.err.println("Error: " + e.getMessage());
+                }
+
+            } else if ("s".equals(command) || "sign up".equalsIgnoreCase(command)) {
+                System.out.print("Name: ");
+                String name = scn.nextLine().trim();
+
+                System.out.print("Email: ");
+                String email = scn.nextLine().trim();
+
+                System.out.print("Password: ");
+                String password = scn.nextLine().trim();
+
+                try {
+                    UserService.registerUser(name, email, password);
+                    System.out.println("Your new account is created.");
+                    System.out.println("Go ahead and login!");
+                } catch (IllegalArgumentException e) {
+                    System.out.println("Error: " + e.getMessage());
+                }
+            } else if ("q".equals(command) || "quit".equalsIgnoreCase(command))
+                break;
+            else {
+                System.out.println("Invalid command. Please try again.");
+            }
+            System.out.println("[L]ogin, [S]ign up, [Q]uit:");
+        }
     }
 }
---
+++ src/main/java/aut/ap/service/UserService.java
@@ -0,0 +1,68 @@
+package aut.ap.service;
+
+import aut.ap.framework.SingletonSessionFactory;
+import aut.ap.model.*;
+
+public class UserService {
+    public static User findByEmail(String email) {
+        return SingletonSessionFactory.get()
+                .fromTransaction(session ->
+                        session.createNativeQuery("select * from users " +
+                                        "where email = :email", User.class)
+                                .setParameter("email", email)
+                                .getResultStream()
+                                .findFirst()
+                                .orElse(null));
+    }
+
+    public static void registerUser(String name, String email, String password) {
+        if (findByEmail(email) != null)
+            throw new IllegalArgumentException("Such email already exists");
+
+        if (email == null || email.isEmpty())
+            throw new IllegalArgumentException("Email cannot be empty");
+
+        if (!email.contains("@"))
+            email = email + "@milou.com";
+
+        if (name == null || name.isEmpty())
+            throw new IllegalArgumentException("Name cannot be empty");
+
+        checkPassword(password);
+
+        String finalEmail = email;
+        SingletonSessionFactory.get()
+                .inTransaction(session ->
+                        session.createNativeMutationQuery("insert into users(name, email, password) " +
+                                        "values (:name, :email, :password)")
+                                .setParameter("name", name)
+                                .setParameter("email", finalEmail)
+                                .setParameter("password", password)
+                                .executeUpdate());
+    }
+
+    public static User loginUser(String email, String password) {
+        checkPassword(password);
+
+        if (email == null || email.isEmpty())
+            throw new IllegalArgumentException("Email cannot be empty");
+
+        if (!email.contains("@"))
+            email = email + "@milou.com";
+
+        if (findByEmail(email) == null)
+            throw new IllegalArgumentException("No such email.\nPlease sign up first!");
+
+        if (!findByEmail(email).getPassword().equals(password))
+            throw new IllegalArgumentException("Wrong password");
+
+        return findByEmail(email);
+    }
+
+    private static void checkPassword(String password) {
+        if (password == null || password.isEmpty())
+            throw new IllegalArgumentException("Password cannot be empty");
+        if (password.length() < 8)
+            throw new IllegalArgumentException("Short password");
+    }
+}
